eb6924991a9882cbd62d62690e91815670e17655
V.Krishn <vkrishn4@gmail.com>
2015-08-24 16:55:09 +0530

    update config database sql, with favicon column

--- config/urls.sqlite.sql
+++ config/urls.sqlite.sql
@@ -1,13 +1,14 @@
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS `rss_url`;
-CREATE TABLE rss_url (
-  rssurl VARCHAR(1024) PRIMARY KEY NOT NULL,
-  tags VARCHAR(1024) NOT NULL DEFAULT "",
-  dbname VARCHAR(64) NOT NULL DEFAULT "default",
-  lastmodified INTEGER(11) NOT NULL DEFAULT 0,
-  lastfetch INTEGER(11) NOT NULL DEFAULT 0,
-  sha1sum INTEGER(40) NOT NULL DEFAULT 0
---   is_rtl INTEGER(1) NOT NULL DEFAULT 0,
---   etag VARCHAR(128) NOT NULL DEFAULT ""
+CREATE TABLE `rss_url` (
+    `rssurl`       VARCHAR(1024) PRIMARY KEY NOT NULL
+  , `tags`         VARCHAR(1024) NOT NULL DEFAULT ""
+  , `dbname`       VARCHAR(64) NOT NULL DEFAULT "default"
+  , `lastmodified` INTEGER(11) NOT NULL DEFAULT 0
+  , `lastfetch`    INTEGER(11) NOT NULL DEFAULT 0
+  , `sha1sum`      INTEGER(40) NOT NULL DEFAULT 0
+  , `icon`         BOOL
+--  , is_rtl       INTEGER(1) NOT NULL DEFAULT 0
+--  , etag         VARCHAR(128) NOT NULL DEFAULT ""
 );
 COMMIT;
